a2dd3a5547d2c3f43d87cfd5a12f673bb36a3da0
Nehemiah Dacres <vivacarlie@gmail.com>
2022-09-10 09:45:32 -0500

    qrc not neeed yet

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
-    engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
+    engine.load(QUrl(QStringLiteral("main.qml")));
     return app.exec();
 }
